9589b40d05f1cfba318f2881f586642c3d40fd84
kalz2q <katsu95i@gmail.com>
2022-09-17 16:53:17 +0900

    mmiddle

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,72 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="FLAG belgium"></div>
+      <div className="FLAG spain"></div>
+      <div className="FLAG czech">
+        <div className="triangle"></div>
+      </div>
+      <div className="FLAG denmark"></div>
+      <div className="FLAG cuba">
+        <div className="triangle">
+          <div className="star-wrap">
+            <div className="star"></div>
+          </div>
+        </div>
+      </div>
+      <div className="FLAG china">
+        <div className="star1">
+          <div className="inner-star"></div>
+        </div>
+        <div className="star2">
+          <div className="inner-star"></div>
+        </div>
+        <div className="star3">
+          <div className="inner-star"></div>
+        </div>
+        <div className="star4">
+          <div className="inner-star"></div>
+        </div>
+        <div className="star5">
+          <div className="inner-star"></div>
+        </div>
+      </div>
+      <div className="FLAG greenland"></div>
+      <div className="FLAG congo">
+        <div className="star">
+          <div className="inner-star"></div>
+        </div>
+      </div>
+      <div className="FLAG qatar">
+        <div className="triangle1"></div>
+        <div className="triangle2"></div>
+        <div className="triangle3"></div>
+        <div className="triangle4"></div>
+        <div className="triangle5"></div>
+        <div className="triangle6"></div>
+        <div className="triangle7"></div>
+        <div className="triangle8"></div>
+      </div>
+      <div className="FLAG jamaica">
+        <div className="triangleL"></div>
+        <div className="triangleR"></div>
+      </div>
+      <div className="FLAG VC">
+        <div className="caro1">
+          <div className="caro"></div>
+        </div>
+        <div className="caro2">
+          <div className="caro"></div>
+        </div>
+        <div className="caro3">
+          <div className="caro"></div>
+        </div>
+      </div>
+      <div className="FLAG scotland"></div>
+      <div className="FLAG pakistan">
+        <div className="star"></div>
+      </div>
+
 
     </div>
   );
